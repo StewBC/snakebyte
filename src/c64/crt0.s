40b8c12cbcb00f62d9a28ce6101eaad65bd4161d
StewBC <swessels@email.com>
2014-03-01 22:14:33 +0100

    Compiler Update req. update
    Update cc65 to V2.14.0 has changed the crt0.s files.

--- src/c64/crt0.s
+++ src/c64/crt0.s
@@ -2,157 +2,143 @@
 ; Startup code for cc65 (C64 version)
 ;
 
-	.export		_exit
+        .export         _exit
         .export         __STARTUP__ : absolute = 1      ; Mark as startup
-	.import		initlib, donelib, callirq
-       	.import	       	zerobss
-	.import	     	callmain
+        .import         initlib, donelib
+        .import         zerobss
+        .import         callmain
         .import         RESTOR, BSOUT, CLRCH
-	.import	       	__INTERRUPTOR_COUNT__
-	.import		__RAM_START__, __RAM_SIZE__	; Linker generated
-	.import		__RODATA_LOAD__, __RODATA_RUN__, __RODATA_SIZE__	; Linker generated
-	.import		__DATA_LOAD__, __DATA_RUN__, __DATA_SIZE__	; Linker generated
-	.import		__STACKSIZE__			; Linker generated
+        .import         __RAM_START__, __RAM_SIZE__     ; Linker generated
+		.import			__RODATA_LOAD__, __RODATA_RUN__, __RODATA_SIZE__	; Linker generated
+		.import			__DATA_LOAD__, __DATA_RUN__, __DATA_SIZE__	; Linker generated
+        .import         __STACKSIZE__                   ; Linker generated
+        .importzp       ST
 
         .include        "zeropage.inc"
-	.include     	"c64.inc"
+        .include        "c64.inc"
+
 
 ; ------------------------------------------------------------------------
 ; Startup code
 
-.segment       	"STARTUP"
+.segment        "STARTUP"
 
 Start:
 
 ; Save the zero page locations we need
 
-        ldx    	#zpspace-1
-L1:	lda	sp,x
-       	sta    	zpsave,x
-	dex
-       	bpl	L1
+        ldx     #zpspace-1
+L1:     lda     sp,x
+        sta     zpsave,x
+        dex
+        bpl     L1
 
 ; Switch to second charset
 
-;	lda	#14
-;	jsr	BSOUT
+;        lda     #14
+;        jsr     BSOUT
 
 ; Switch off the BASIC ROM
 
-	lda	$01
-       	pha                     ; Remember the value
-	and	#$F8
-       	ora	#$06		; Enable kernal+I/O, disable basic
-	sta	$01
+        lda     $01
+        pha                     ; Remember the value
+        and     #$F8
+        ora     #$06            ; Enable kernal+I/O, disable basic
+        sta     $01
 
 ; Clear the BSS data
 
-	jsr	zerobss
-	
-; Move DATA into place
+        jsr     zerobss
 
-	lda	#<__DATA_RUN__
-	sta	ptr1
-	lda	#>__DATA_RUN__
-	sta	ptr1+1
-	lda #<__DATA_LOAD__
-	sta ptr2
-	lda #>__DATA_LOAD__
-	sta ptr2+1
-	lda	#0
-	tay
+		; Move DATA into place
 
-; Move full pages
+			lda	#<__DATA_RUN__
+			sta	ptr1
+			lda	#>__DATA_RUN__
+			sta	ptr1+1
+			lda #<__DATA_LOAD__
+			sta ptr2
+			lda #>__DATA_LOAD__
+			sta ptr2+1
+			lda	#0
+			tay
 
-	ldx	#>__DATA_SIZE__
-	beq	D3
-D2:	lda (ptr2),y
-	sta	(ptr1),y
-   	iny
-   	bne	D2
-	inc	ptr1+1
-	inc ptr2+1
-	dex
-	bne	D2
+		; Move full pages
 
-; Move remaining page (y is zero on entry)
+			ldx	#>__DATA_SIZE__
+			beq	D3
+		D2:	lda (ptr2),y
+			sta	(ptr1),y
+		   	iny
+		   	bne	D2
+			inc	ptr1+1
+			inc ptr2+1
+			dex
+			bne	D2
 
-D3:	cpy	#<__DATA_SIZE__
-	beq	D4
-	lda (ptr2),y
-	sta	(ptr1),y
-	iny
-	bne	D3
-D4:
+		; Move remaining page (y is zero on entry)
 
-; Move RODATA into place
+		D3:	cpy	#<__DATA_SIZE__
+			beq	D4
+			lda (ptr2),y
+			sta	(ptr1),y
+			iny
+			bne	D3
+		D4:
 
-	lda	#<__RODATA_RUN__
-	sta	ptr1
-	lda	#>__RODATA_RUN__
-	sta	ptr1+1
-	lda #<__RODATA_LOAD__
-	sta ptr2
-	lda #>__RODATA_LOAD__
-	sta ptr2+1
-	lda	#0
-	tay
+		; Move RODATA into place
 
-; Move full pages
+			lda	#<__RODATA_RUN__
+			sta	ptr1
+			lda	#>__RODATA_RUN__
+			sta	ptr1+1
+			lda #<__RODATA_LOAD__
+			sta ptr2
+			lda #>__RODATA_LOAD__
+			sta ptr2+1
+			lda	#0
+			tay
 
-	ldx #>__RODATA_SIZE__
-	beq	RO3
-RO2:lda (ptr2),y
-	sta	(ptr1),y
-   	iny
-   	bne	RO2
-	inc	ptr1+1
-	inc ptr2+1
-	dex
-	bne	RO2
+		; Move full pages
 
-; Move remaining page (y is zero on entry)
+			ldx #>__RODATA_SIZE__
+			beq	RO3
+		RO2:lda (ptr2),y
+			sta	(ptr1),y
+		   	iny
+		   	bne	RO2
+			inc	ptr1+1
+			inc ptr2+1
+			dex
+			bne	RO2
 
-RO3:cpy	#<__RODATA_SIZE__
-	beq	RO4
-	lda (ptr2),y
-	sta	(ptr1),y
-	iny
-	bne	RO3
-RO4:
-	
+		; Move remaining page (y is zero on entry)
+
+		RO3:cpy	#<__RODATA_SIZE__
+			beq	RO4
+			lda (ptr2),y
+			sta	(ptr1),y
+			iny
+			bne	RO3
+		RO4:
+
 
 ; Save system settings and setup the stack
 
         pla
-        sta	mmusave      	; Save the memory configuration
+        sta     mmusave         ; Save the memory configuration
 
-       	tsx
-       	stx    	spsave 		; Save the system stack ptr
+        tsx
+        stx     spsave          ; Save the system stack ptr
 
-	lda    	#<(__RAM_START__ + __RAM_SIZE__ + __STACKSIZE__)
-	sta	sp
-	lda	#>(__RAM_START__ + __RAM_SIZE__ + __STACKSIZE__)
-       	sta	sp+1   		; Set argument stack ptr
-
-; If we have IRQ functions, chain our stub into the IRQ vector
-
-        lda     #<__INTERRUPTOR_COUNT__
-      	beq	NoIRQ1
-      	lda	IRQVec
-       	ldx	IRQVec+1
-      	sta	IRQInd+1
-      	stx	IRQInd+2
-      	lda	#<IRQStub
-      	ldx	#>IRQStub
-      	sei
-      	sta	IRQVec
-      	stx	IRQVec+1
-      	cli
+        lda     #<(__RAM_START__ + __RAM_SIZE__ + __STACKSIZE__)
+        sta     sp
+        lda     #>(__RAM_START__ + __RAM_SIZE__ + __STACKSIZE__)
+        sta     sp+1            ; Set argument stack ptr
 
 ; Call module constructors
 
-NoIRQ1:	jsr	initlib
+        jsr     initlib
 
 ; Push arguments and call main
 
@@ -160,65 +146,41 @@ NoIRQ1:	jsr	initlib
 
 ; Back from main (This is also the _exit entry). Run module destructors
 
-_exit:  jsr	donelib
-
-
-; Reset the IRQ vector if we chained it.
-
-        pha  			; Save the return code on stack
-	lda     #<__INTERRUPTOR_COUNT__
-	beq	NoIRQ2
-	lda	IRQInd+1
-	ldx	IRQInd+2
-	sei
-	sta	IRQVec
-	stx	IRQVec+1
-	cli
+_exit:  pha                     ; Save the return code on stack
+        jsr     donelib
 
 ; Copy back the zero page stuff
 
-NoIRQ2: ldx	#zpspace-1
-L2:   	lda	zpsave,x
-      	sta	sp,x
-      	dex
-       	bpl	L2
+        ldx     #zpspace-1
+L2:     lda     zpsave,x
+        sta     sp,x
+        dex
+        bpl     L2
 
 ; Place the program return code into ST
 
-	pla
-	sta	ST
+        pla
+        sta     ST
 
 ; Restore system stuff
 
-  	ldx	spsave
-	txs	       	  	; Restore stack pointer
-       	ldx    	mmusave
-	stx	$01    	  	; Restore memory configuration
+        ldx     spsave
+        txs                     ; Restore stack pointer
+        ldx     mmusave
+        stx     $01             ; Restore memory configuration
 
-; Reset changed vectors, back to basic
+; Back to basic
 
-	jmp	RESTOR
-
-; ------------------------------------------------------------------------
-; The IRQ vector jumps here, if condes routines are defined with type 2.
-
-IRQStub:
-	cld    	       		   	; Just to be sure
-       	jsr    	callirq                 ; Call the functions
-       	jmp    	IRQInd			; Jump to the saved IRQ vector
+        rts
 
 ; ------------------------------------------------------------------------
 ; Data
 
-.data
-
-IRQInd: jmp     $0000
-
 .segment        "ZPSAVE"
 
-zpsave:	.res	zpspace
+zpsave: .res    zpspace
 
 .bss
 
-spsave:	.res	1
-mmusave:.res	1
+spsave: .res    1
+mmusave:.res    1
--- src/c64text/crt0.s
+++ src/c64text/crt0.s
@@ -1,83 +1,69 @@
 ;
-; Startup code for cc65 (C64 version) - c64text
+; Startup code for cc65 (C64 version)
 ;
 
-	.export		_exit
+        .export         _exit
         .export         __STARTUP__ : absolute = 1      ; Mark as startup
-	.import		initlib, donelib, callirq
-       	.import	       	zerobss
-	.import	     	callmain
+        .import         initlib, donelib
+        .import         zerobss
+        .import         callmain
         .import         RESTOR, BSOUT, CLRCH
-	.import	       	__INTERRUPTOR_COUNT__
-	.import		__RAM_START__, __RAM_SIZE__	; Linker generated
-	.import		__STACKSIZE__			; Linker generated
+        .import         __RAM_START__, __RAM_SIZE__     ; Linker generated
+        .import         __STACKSIZE__                   ; Linker generated
+        .importzp       ST
 
         .include        "zeropage.inc"
-	.include     	"c64.inc"
+        .include        "c64.inc"
+
 
 ; ------------------------------------------------------------------------
 ; Startup code
 
-.segment       	"STARTUP"
+.segment        "STARTUP"
 
 Start:
 
 ; Save the zero page locations we need
 
-        ldx    	#zpspace-1
-L1:	lda	sp,x
-       	sta    	zpsave,x
-	dex
-       	bpl	L1
+        ldx     #zpspace-1
+L1:     lda     sp,x
+        sta     zpsave,x
+        dex
+        bpl     L1
 
 ; Switch to second charset
 
-;	lda	#14
-;	jsr	BSOUT
+        ; lda     #14
+        ; jsr     BSOUT
 
 ; Switch off the BASIC ROM
 
-	lda	$01
-       	pha                     ; Remember the value
-	and	#$F8
-       	ora	#$06		; Enable kernal+I/O, disable basic
-	sta	$01
+        lda     $01
+        pha                     ; Remember the value
+        and     #$F8
+        ora     #$06            ; Enable kernal+I/O, disable basic
+        sta     $01
 
 ; Clear the BSS data
 
-	jsr	zerobss
-	
+        jsr     zerobss
+
 ; Save system settings and setup the stack
 
         pla
-        sta	mmusave      	; Save the memory configuration
+        sta     mmusave         ; Save the memory configuration
 
-       	tsx
-       	stx    	spsave 		; Save the system stack ptr
+        tsx
+        stx     spsave          ; Save the system stack ptr
 
-	lda    	#<(__RAM_START__ + __RAM_SIZE__ + __STACKSIZE__)
-	sta	sp
-	lda	#>(__RAM_START__ + __RAM_SIZE__ + __STACKSIZE__)
-       	sta	sp+1   		; Set argument stack ptr
-
-; If we have IRQ functions, chain our stub into the IRQ vector
-
-        lda     #<__INTERRUPTOR_COUNT__
-      	beq	NoIRQ1
-      	lda	IRQVec
-       	ldx	IRQVec+1
-      	sta	IRQInd+1
-      	stx	IRQInd+2
-      	lda	#<IRQStub
-      	ldx	#>IRQStub
-      	sei
-      	sta	IRQVec
-      	stx	IRQVec+1
-      	cli
+        lda     #<(__RAM_START__ + __RAM_SIZE__ + __STACKSIZE__)
+        sta     sp
+        lda     #>(__RAM_START__ + __RAM_SIZE__ + __STACKSIZE__)
+        sta     sp+1            ; Set argument stack ptr
 
 ; Call module constructors
 
-NoIRQ1:	jsr	initlib
+        jsr     initlib
 
 ; Push arguments and call main
 
@@ -85,65 +71,41 @@ NoIRQ1:	jsr	initlib
 
 ; Back from main (This is also the _exit entry). Run module destructors
 
-_exit:  jsr	donelib
-
-
-; Reset the IRQ vector if we chained it.
-
-        pha  			; Save the return code on stack
-	lda     #<__INTERRUPTOR_COUNT__
-	beq	NoIRQ2
-	lda	IRQInd+1
-	ldx	IRQInd+2
-	sei
-	sta	IRQVec
-	stx	IRQVec+1
-	cli
+_exit:  pha                     ; Save the return code on stack
+        jsr     donelib
 
 ; Copy back the zero page stuff
 
-NoIRQ2: ldx	#zpspace-1
-L2:   	lda	zpsave,x
-      	sta	sp,x
-      	dex
-       	bpl	L2
+        ldx     #zpspace-1
+L2:     lda     zpsave,x
+        sta     sp,x
+        dex
+        bpl     L2
 
 ; Place the program return code into ST
 
-	pla
-	sta	ST
+        pla
+        sta     ST
 
 ; Restore system stuff
 
-  	ldx	spsave
-	txs	       	  	; Restore stack pointer
-       	ldx    	mmusave
-	stx	$01    	  	; Restore memory configuration
+        ldx     spsave
+        txs                     ; Restore stack pointer
+        ldx     mmusave
+        stx     $01             ; Restore memory configuration
 
-; Reset changed vectors, back to basic
+; Back to basic
 
-	jmp	RESTOR
-
-; ------------------------------------------------------------------------
-; The IRQ vector jumps here, if condes routines are defined with type 2.
-
-IRQStub:
-	cld    	       		   	; Just to be sure
-       	jsr    	callirq                 ; Call the functions
-       	jmp    	IRQInd			; Jump to the saved IRQ vector
+        rts
 
 ; ------------------------------------------------------------------------
 ; Data
 
-.data
-
-IRQInd: jmp     $0000
-
 .segment        "ZPSAVE"
 
-zpsave:	.res	zpspace
+zpsave: .res    zpspace
 
 .bss
 
-spsave:	.res	1
-mmusave:.res	1
+spsave: .res    1
+mmusave:.res    1
